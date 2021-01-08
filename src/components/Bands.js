a6602195bd543dfb367a072f7fdd616734a86ba7
Marc vd Made <marcvdmade01@gmail.com>
2021-01-08 17:40:43 +0100

    added delete and add band

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,5 +1,8 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export class Bands extends React.Component {
     constructor() {
@@ -7,26 +10,96 @@ export class Bands extends React.Component {
 
         this.state = {
             jsonLoaded: false,
-            bands: []
+            bands: [],
+            showCreateModal: false,
+            name: "",
+            rating: "",
+            mainGenre: "",
         }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.addBand = this.addBand.bind(this)
     }
 
     async loadJSON() {
         try {
-            await fetch('http://145.24.222.171:8000/api/bands', {
+            let response = await fetch('http://145.24.222.171:8000/api/bands', {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json'
                 }
             })
-                .then(response => response.json())
-                .then(data => {
-                    this.setState({
-                        jsonLoaded: true,
-                        bands: data.items,
-                    })
-                })
-                .catch(err => console.error(err))
+
+            let json = await response.json()
+
+            json.items.map(band => (
+                band.hidden = true
+            ))         
+
+            console.log(json)
+
+            this.setState({
+                jsonLoaded: true,
+                bands: json.items
+            })
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    handleCreateModal() {
+        this.setState({
+            showCreateModal: !this.state.showCreateModal
+        })
+    }
+
+    handleInputChange(e) {
+
+        const target = e.target
+        const value = target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    async addBand() {
+        let values = {
+            name: this.state.name,
+            rating: this.state.rating,
+            mainGenre: this.state.mainGenre
+        }
+
+        try {
+            await fetch('http://145.24.222.171:8000/api/bands', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
+            alert("Added a new band")
+            this.loadJSON()
+        } catch(err) {
+            console.error(err)
+        }
+    }
+    
+
+    async deleteBand(id) {
+        try {
+            await fetch('http://145.24.222.171:8000/api/bands/' + id, {
+                method: 'DELETE',
+                mode: 'cors',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            alert("Deleted a band")
+            this.loadJSON()
         } catch (err) {
             console.error(err)
         }
@@ -36,12 +109,8 @@ export class Bands extends React.Component {
         this.loadJSON()
     }
 
-    componentDidUpdate() {
-        this.loadJSON()
-    }
-
     render() {
-        const { jsonLoaded, bands} = this.state;
+        const { jsonLoaded, bands } = this.state;
 
         if (!jsonLoaded) {
             return <h2 className="app container text-center">Loading bands...</h2>
@@ -50,20 +119,72 @@ export class Bands extends React.Component {
                 <div key={band._id} className="card m-3">
                     <div className="card-header d-flex justify-content-around">
                         <h2>{band.name}</h2>
+                        <button className="btn"><FontAwesomeIcon icon={faSortDown} color="black" /></button>
                     </div>
-                    <div className="card-body">
-                        <p>Hier komt extra info over band</p>
+
+                    <div style={{display: (band.hidden ? 'none' : 'block')}} className="card-body">
+                        <p>Rating: {band.rating}</p>
+                        <p>Genre: {band.mainGenre}</p>
                         <div className="d-flex justify-content-around">
-                            <button className="btn btn-primary">Edit Band</button>
-                            <button className="btn btn-danger">Delete Band</button>
+                            <button className="btn btn-primary m-2">Edit Band</button>
+                            <button onClick={() => this.deleteBand(band._id)} className="btn btn-danger m-2">Delete Band</button>
                         </div>
                     </div>
+
                 </div>
             ))
             return (
                 <div className="Bands mb-5">
                     <div>
-                        <button type="submit" className="btn btn-primary">Create Band</button>
+                        <h2>Amount of rated bands: {this.state.bands.length}</h2>
+                        <Button onClick={() => this.handleCreateModal()}>Add new band</Button>
+
+                        <Modal
+                            show={this.state.showCreateModal}
+                            size="lg"
+                            aria-labelledby="contained-modal-title-vcenter"
+                            centered
+                        >
+                            <Modal.Header>
+                                <Modal.Title>Add a new band</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form onSubmit={(e) => {
+                                    e.preventDefault()
+                                    this.addBand()
+
+                                    this.setState({
+                                        name: '',
+                                        rating: '',
+                                        mainGenre: ''
+                                    })
+
+                                    this.handleCreateModal()
+                                }}>
+                                    <Form.Group>
+                                        <Form.Label>Band</Form.Label>
+                                        <Form.Control name="name" onChange={this.handleInputChange} value={this.state.name} type="text" required />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Rating</Form.Label>
+                                        <Form.Control name="rating" onChange={this.handleInputChange} value={this.state.rating} type="number" min="1" max="10" required />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Main Genre</Form.Label>
+                                        <Form.Control name="mainGenre" onChange={this.handleInputChange} value={this.state.mainGenre} type="text" required />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Add Band
+                                    </Button>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={() => this.handleCreateModal()}>Cancel</Button>
+                            </Modal.Footer>
+                        </Modal>
+
                     </div>
                     <div className="container">
                         <div className="row justify-content-center">
