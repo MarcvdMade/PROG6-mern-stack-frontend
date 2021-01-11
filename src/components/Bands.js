7b3bed4a419eb7dde2192987dc19f4ccc570accf
Marc vd Made <marcvdmade01@gmail.com>
2021-01-11 15:52:11 +0100

    made edit and detail

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,8 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 import React from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import '../App.css';
 
 export class Bands extends React.Component {
     constructor() {
@@ -11,14 +10,19 @@ export class Bands extends React.Component {
         this.state = {
             jsonLoaded: false,
             bands: [],
+            pagination: [],
             showCreateModal: false,
+            showEditModal: false,
             name: "",
             rating: "",
             mainGenre: "",
+            editBand: [],
+            editName: "",
+            editRating: "",
+            editMainGenre: ""
         }
 
         this.handleInputChange = this.handleInputChange.bind(this)
-        this.addBand = this.addBand.bind(this)
     }
 
     async loadJSON() {
@@ -32,18 +36,36 @@ export class Bands extends React.Component {
 
             let json = await response.json()
 
-            json.items.map(band => (
-                band.hidden = true
-            ))         
+            // json.items.map(band => (
+            //     band.hidden = true
+            // ))         
 
             console.log(json)
 
             this.setState({
                 jsonLoaded: true,
-                bands: json.items
+                bands: json.items,
+                pagination: json.pagination
             })
         } catch (err) {
             console.error(err)
+        }
+    }
+
+    handleShowDetail(e) {
+        console.log(e.target)
+        let t = e.target
+        let div = t.parentElement.parentElement.children
+        console.log(div[1])
+
+        if (div[1].classList.contains("card-show")) {
+            t.innerHTML = "Show Details"
+            div[1].classList.remove("card-show")
+            div[1].classList.add("card-hidden")
+        } else {
+            t.innerHTML = "Hide Details"
+            div[1].classList.add("card-show")
+            div[1].classList.remove("card-hidden")
         }
     }
 
@@ -62,6 +84,30 @@ export class Bands extends React.Component {
         this.setState({
             [name]: value
         })
+    }
+
+    async editBand() {
+        let values = {
+            name: this.state.editName,
+            rating: this.state.editRating,
+            mainGenre: this.state.editMainGenre
+        }
+
+        try {
+            await fetch(this.state.editBand._links.self.href, {
+                method: 'PUT',
+                mode: 'cors',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
+            alert("Edited Band")
+            this.loadJSON()
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     async addBand() {
@@ -83,15 +129,24 @@ export class Bands extends React.Component {
             })
             alert("Added a new band")
             this.loadJSON()
-        } catch(err) {
+        } catch (err) {
             console.error(err)
         }
     }
-    
 
-    async deleteBand(id) {
+    handleEditModal(band) {
+        this.setState({
+            editBand: band,
+            editName: band.name,
+            editRating: band.rating,
+            editMainGenre: band.mainGenre,
+            showEditModal: !this.state.showEditModal
+        })
+    }
+
+    async deleteBand(uri) {
         try {
-            await fetch('http://145.24.222.171:8000/api/bands/' + id, {
+            await fetch(uri, {
                 method: 'DELETE',
                 mode: 'cors',
                 headers: {
@@ -117,17 +172,17 @@ export class Bands extends React.Component {
         } else {
             let bandItems = bands.map(band => (
                 <div key={band._id} className="card m-3">
-                    <div className="card-header d-flex justify-content-around">
+                    <div className="card-header d-flex flex-column justify-content-around">
                         <h2>{band.name}</h2>
-                        <button className="btn"><FontAwesomeIcon icon={faSortDown} color="black" /></button>
+                        <button onClick={this.handleShowDetail} className="btn">Show Details</button>
                     </div>
 
-                    <div style={{display: (band.hidden ? 'none' : 'block')}} className="card-body">
+                    <div className="card-body card-hidden">
                         <p>Rating: {band.rating}</p>
                         <p>Genre: {band.mainGenre}</p>
                         <div className="d-flex justify-content-around">
-                            <button className="btn btn-primary m-2">Edit Band</button>
-                            <button onClick={() => this.deleteBand(band._id)} className="btn btn-danger m-2">Delete Band</button>
+                            <button onClick={() => this.handleEditModal(band)} className="btn btn-primary m-2">Edit Band</button>
+                            <button onClick={() => this.deleteBand(band._links.self.href)} className="btn btn-danger m-2">Delete Band</button>
                         </div>
                     </div>
 
@@ -136,7 +191,7 @@ export class Bands extends React.Component {
             return (
                 <div className="Bands mb-5">
                     <div>
-                        <h2>Amount of rated bands: {this.state.bands.length}</h2>
+                        <h2>Amount of rated bands/artist: {this.state.bands.length}</h2>
                         <Button onClick={() => this.handleCreateModal()}>Add new band</Button>
 
                         <Modal
@@ -167,8 +222,8 @@ export class Bands extends React.Component {
                                     </Form.Group>
 
                                     <Form.Group>
-                                        <Form.Label>Rating</Form.Label>
-                                        <Form.Control name="rating" onChange={this.handleInputChange} value={this.state.rating} type="number" min="1" max="10" required />
+                                        <Form.Label>Rating: {this.state.rating}</Form.Label>
+                                        <Form.Control name="rating" onChange={this.handleInputChange} value={this.state.rating} type="range" min="1" max="10" required />
                                     </Form.Group>
 
                                     <Form.Group>
@@ -185,11 +240,63 @@ export class Bands extends React.Component {
                             </Modal.Footer>
                         </Modal>
 
+                        <Modal
+                            show={this.state.showEditModal}
+                            size="lg"
+                            aria-labelledby="contained-modal-title-vcenter"
+                            centered
+                        >
+                            <Modal.Header>
+                                <Modal.Title>Edit</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form onSubmit={(e) => {
+                                    e.preventDefault()
+                                    this.editBand()
+
+                                    this.setState({
+                                        editBand: [],
+                                        editName: "",
+                                        editRating: "",
+                                        editMainGenre: "",
+                                        showEditModal: !this.state.showEditModal
+                                    })
+                                }}>
+                                    <Form.Group>
+                                        <Form.Label>Edit {this.state.editName}</Form.Label>
+                                        <Form.Control name="editName" onChange={this.handleInputChange} value={this.state.editName} type="text" required />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Rating: {this.state.editRating}</Form.Label>
+                                        <Form.Control name="editRating" onChange={this.handleInputChange} value={this.state.editRating} type="range" min="1" max="10" required />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Main Genre</Form.Label>
+                                        <Form.Control name="editMainGenre" onChange={this.handleInputChange} value={this.state.editMainGenre} type="text" required />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Edit band
+                                    </Button>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={() => {
+                                    this.setState({
+                                        editBand: [],
+                                        editName: "",
+                                        editRating: "",
+                                        editMainGenre: "",
+                                        showEditModal: !this.state.showEditModal
+                                    })
+                                }}>Cancel</Button>
+                            </Modal.Footer>
+                        </Modal>
+
                     </div>
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            {bandItems}
-                        </div>
+                    <div className="d-flex flex-column">
+                        {bandItems}
                     </div>
                 </div>
             )
